f5c48eee432405fd6a5fda29ebb1851c015850d8
Bak8 <ataibegaliev@gmail.com>
2022-09-19 21:34:00 +0600

    dwa

--- Lesson 4 Cycle/main.swift
+++ Lesson 4 Cycle/main.swift
@@ -1,11 +1,50 @@
-//
-//  main.swift
-//  Lesson 4 Cycle
-//
-//  Created by Atai Begaliev on 19/9/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+//print("Type your number")
+//let readline = readLine()!
+//
+//for item in 1...10 {
+//    print("\(item) * \(Int(readline)!) = \(item * Int(readline)!)")
+//}
+
+//1...10 - 10 включенно
+//1..<10 - До 10 (до 9)
+
+//for symbol in "ABCDEF"{
+//    print(symbol)
+//}
+//
+//for item in 1...100 {
+//    if item % 2 == 0{
+//    print(item)
+//    }
+//}
+//
+////альтернатива
+//for item in 1...100 where item % 2 == 0 {
+//    print(item)
+//}
+//
+//
+//for itemtwo in 1...200 {
+//    if itemtwo % 10 == 5 {
+//    print(itemtwo)
+//    }
+//}
+
+var readline = readLine()!
+var ESstring = ""
+
+for _ in readline {
+    ESstring += "1"
+}
+print(ESstring)
+
+//for item in 1...100 {
+//    if item % 2 == 0{
+//    print(item)
+//    }
+//}
+
+
 
